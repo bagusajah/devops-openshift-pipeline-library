b7c1666e52622d9eb3690a6be358bfeaefe14f5a
Jesada Gonkratoke <jesada.gon@ascendcorp.com>
2018-03-08 16:21:26 +0700

    initial project

---
+++ src/main/groovy/com/ascendmoney/devops/DevopsApplication.groovy
@@ -0,0 +1,12 @@
+package com.ascendmoney.devops
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DevopsApplication, args
+	}
+}
