eb1e8a645b816ec3665d1df38b7f4ee6ba46ce7b
Jesada Gonkratoke <jesada.gon@ascendcorp.com>
2018-03-09 17:18:04 +0700

    re-initial

--- src/main/groovy/com/ascendmoney/vulcan/VulcanApplication.groovy
+++ src/main/groovy/com/ascendmoney/vulcan/VulcanApplication.groovy
@@ -1,12 +1,12 @@
-package com.ascendmoney.devops
+package com.ascendmoney.vulcan
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class DevopsApplication {
+class VulcanApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run DevopsApplication, args
+		SpringApplication.run VulcanApplication, args
 	}
 }
--- src/test/groovy/com/ascendmoney/vulcan/VulcanApplicationTests.groovy
+++ src/test/groovy/com/ascendmoney/vulcan/VulcanApplicationTests.groovy
@@ -1,4 +1,4 @@
-package com.ascendmoney.devops
+package com.ascendmoney.vulcan
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner)
 @SpringBootTest
-class DevopsApplicationTests {
+class VulcanApplicationTests {
 
 	@Test
 	void contextLoads() {
