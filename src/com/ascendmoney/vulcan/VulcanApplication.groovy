690e199ee339c93c24f2ae5306c05d20c093b032
Jesada Gonkratoke <jesada.gon@ascendcorp.com>
2018-03-12 15:48:07 +0700

    update

--- src/com/ascendmoney/vulcan/VulcanApplication.groovy
+++ src/com/ascendmoney/vulcan/VulcanApplication.groovy
@@ -1,8 +1,6 @@
 package com.ascendmoney.vulcan
 
 import com.cloudbees.groovy.cps.NonCPS
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
 
 def sayHello() {
     return "Hi, I am vulcan"
