8120e4fc14f7b0973900bd0d222298ee7b897088
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-13 18:24:08 +0700

    update image name

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -5,7 +5,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('maven')
     def label = parameters.get('label', defaultLabel)
 
-    def mavenImage = parameters.get('mavenImage', 'ascendcorphub/maven-builder:v1.0.0')
+    def mavenImage = parameters.get('mavenImage', 'vulcanhub/maven-builder:v1.0.0')
     def robotImage = parameters.get('robotImage', 'ascendcorphub/robot:v1.1.0')
     def jnlpImage = 'docker.io/openshift/jenkins-agent-maven-35-centos7:v3.10'
 
