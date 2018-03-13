71ad3f86afebb4536bb11d78a1d3b7345aa17012
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-13 12:00:09 +0700

    delete buildId and getRepoName from mavenNode for use its by call library

--- vars/mavenNode.groovy
+++ vars/mavenNode.groovy
@@ -11,24 +11,3 @@ def call(Map parameters = [:], body) {
         }
     }
 }
-
-def buildId (prefix){
-    def repo = getRepoName()
-    return "${prefix}${repo}_${env.BUILD_NUMBER}".replaceAll('-', '_').replaceAll('/', '_').replaceAll(' ', '_')
-}
-
-def getRepoName(){
-
-  def jobName = env.JOB_NAME
-
-  // job name from the org plugin
-  if (jobName.count('/') > 1){
-    return jobName.substring(jobName.indexOf('/')+1, jobName.lastIndexOf('/'))
-  }
-  // job name from the branch plugin
-  if (jobName.count('/') > 0){
-    return jobName.substring(0, jobName.lastIndexOf('/'))
-  }
-  // normal job name
-  return jobName
-}
