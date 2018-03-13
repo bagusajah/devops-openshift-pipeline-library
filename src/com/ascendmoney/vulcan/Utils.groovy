ecb66e59b782004e4a02a15c52453b4390b9784e
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-13 11:59:01 +0700

    add getRepoName in Utils and create new lib (buildId)

--- src/com/ascendmoney/vulcan/Utils.groovy
+++ src/com/ascendmoney/vulcan/Utils.groovy
@@ -5,3 +5,19 @@ import com.cloudbees.groovy.cps.NonCPS
 def sayHello() {
     return "Hi, I am vulcan"
 }
+
+def getRepoName(){
+
+  def jobName = env.JOB_NAME
+
+  // job name from the org plugin
+  if (jobName.count('/') > 1){
+    return jobName.substring(jobName.indexOf('/')+1, jobName.lastIndexOf('/'))
+  }
+  // job name from the branch plugin
+  if (jobName.count('/') > 0){
+    return jobName.substring(0, jobName.lastIndexOf('/'))
+  }
+  // normal job name
+  return jobName
+}
