c149e101a1b8f56d95e3f9e6f72c3d8cf2bb138d
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-16 10:04:18 +0700

    update

--- vars/acnCompressConfiguration.groovy
+++ vars/acnCompressConfiguration.groovy
@@ -19,33 +19,32 @@ def call(body){
   def test_pwd = pwd()
   echo "TEST_PWD ${test_pwd}"
 
-  // dir("${directory}/update-config") {
-  //   git credentialsId: 'bitbucket-credential', url: GLOBAL_VARS['GIT_FABRIC8_CONFIGURATION']
-  //   GIT_HASH_FABRIC8_CONFIGURATION = sh script: "cd /home/jenkins/workspace/${env.JOB_NAME}/update-config && git rev-parse --verify HEAD", returnStdout: true
-  //   GIT_HASH_FABRIC8_CONFIGURATION = GIT_HASH_FABRIC8_CONFIGURATION.trim()
-  //   def env_list = ""
-  //   for(n = 0; n < LIST_ENV.size(); n++){
-  //     env_list = LIST_ENV[n]
-  //     sh "mkdir -p /home/jenkins/workspace/${env.JOB_NAME}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}"
-  //     sh "cp -rf /home/jenkins/workspace/${env.JOB_NAME}/update-config/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/* /home/jenkins/workspace/${env.JOB_NAME}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/"
-  //     sh "cd /home/jenkins/workspace/${env.JOB_NAME}/update-config/tmp/${env_list} && /bin/tar -zcvf \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip\" \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/\""
-  //     dir("/home/jenkins/workspace/${env.JOB_NAME}/update-config/tmp/${env_list}"){
-  //       step([
-  //         $class : 'S3BucketPublisher',
-  //         profileName : 'fabric8-profile-s3',
-  //         entries: [[
-  //           bucket: "acm-aws-fabric8-configuration-repo/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}",
-  //           selectedRegion: 'ap-southeast-1',
-  //           showDirectlyInBrowser: true,
-  //           sourceFile: "${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip",
-  //           storageClass: 'STANDARD'
-  //         ]]
-  //       ])
-  //     } // End Upload zip file to s3
-  //   } // End Loop zip file and upload to s3
-  // } // End Scope init git config fabric8
+  git credentialsId: 'bitbucket-credential', url: GLOBAL_VARS['GIT_FABRIC8_CONFIGURATION']  
+  GIT_HASH_FABRIC8_CONFIGURATION = sh script: "cd ${directory}/update-config && git rev-parse --verify HEAD", returnStdout: true
+  GIT_HASH_FABRIC8_CONFIGURATION = GIT_HASH_FABRIC8_CONFIGURATION.trim()
 
-  // return GIT_HASH_FABRIC8_CONFIGURATION
-  return "eiei"
+  def env_list = ""
+  for(n = 0; n < LIST_ENV.size(); n++){
+    env_list = LIST_ENV[n]
+    dir("${directory}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}"){
+      sh "cp -rf ${directory}/update-config/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/* ${directory}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/"
+      sh "cd ${directory}/update-config/tmp/${env_list} && /bin/tar -zcvf \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip\" \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/\""
+      dir("${directory}/update-config/tmp/${env_list}"){
+        step([
+          $class : 'S3BucketPublisher',
+          profileName : 'openshift-profile-s3',
+          entries: [[
+            bucket: "acm-aws-fabric8-configuration-repo/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}",
+            selectedRegion: 'ap-southeast-1',
+            showDirectlyInBrowser: true,
+            sourceFile: "${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip",
+            storageClass: 'STANDARD'
+          ]]
+        ])
+      } // End Upload zip file to s3
+    } // End scope
+  } // End Loop zip file and upload to s3
+
+  return GIT_HASH_FABRIC8_CONFIGURATION
 
 } // End Compress Configuration
