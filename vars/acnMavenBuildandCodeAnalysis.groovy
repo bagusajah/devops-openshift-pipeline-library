859d7aea7bc7c4b16c4ef652f11eb5e403d701c4
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-16 11:20:16 +0700

    init build maven

---
+++ vars/acnMavenBuildandCodeAnalysis.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/groovy
+import com.ascendmoney.vulcan.Utils
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    def appName = config.APPNAME
+    def newVersion = config.VERSION
+    def pushArtifactToNexus = config.pushArtifactToNexus
+    
+    def command_maven = "package"
+    if ( pushArtifactToNexus == "true" ) {
+        command_maven = "deploy"
+    } // End Condition command mvn for push to nexus
+
+    def skipTests = config.skipTests ?: false
+
+    echo "=============== TEMP : WAITING SONARQUBE SERVER ==============="
+    sh "mvn versions:set -DnewVersion=${newVersion}"
+    echo "command_maven ${command_maven}"
+    // sh "mvn clean ${command_maven}"
+
+    echo "=============== DONT FORGET ENABLE SONARQUBE AFTER SONARQUBE SERVER IS ACTIVE ==============="
+    // withSonarQubeEnv('sonarqube') {
+    //     sh "mvn versions:set -DnewVersion=${newVersion}"
+    //     sh 'mvn clean ${command_maven} sonar:sonar ' +
+    //     "-Dsonar.host.url=${config.SONARQUBE_URL} " +
+    //     "-Dsonar.projectKey=${appName} " +
+    //     "-Dsonar.projectName=${appName} " +
+    //     "-Dsonar.projectVersion=${newVersion} " +
+    //     '-Dsonar.language=java ' +
+    //     '-Dsonar.sources=src/ '+
+    //     '-Dsonar.tests=src/test/ '+
+    //     '-Dsonar.test.inclusions=**/*Test*/** '+
+    //     '-Dsonar.exclusions=**/*Test*/**'+
+    //     '-Dsonar.java.binaries=target/classes '
+    // }
+  } // End Function
