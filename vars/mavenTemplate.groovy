0a7d1195fc94916b9bc14f797e76932144e165f1
Jesada Gonkratoke <jesada.gon@ascendcorp.com>
2018-03-16 09:31:54 +0700

    update

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -8,26 +8,26 @@ def call(Map parameters = [:], body) {
     def mavenImage = parameters.get('mavenImage', 'vulcanhub/maven-builder:v1.0.0')
     def robotImage = parameters.get('robotImage', 'ascendcorphub/robot:v1.1.0')
     def jnlpImage = 'docker.io/openshift/jenkins-agent-maven-35-centos7:v3.10'
-    def inheritFrom = parameters.get('inheritFrom', 'base')
+    //def inheritFrom = parameters.get('inheritFrom', 'base')
 
     echo "=========================== Image building using buildconfig on openshift ==========================="
     podTemplate(
         cloud: 'openshift',
-        label: label, 
-        inheritFrom: "${inheritFrom}",
+        label: label,
         serviceAccount: 'jenkins', 
         restartPolicy: 'OnFailure', 
         nodeSelector: 'deployment-nodegroup=vulcan',
         containers: [
-            // [
-            //     name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins/', resourceLimitMemory: '512Mi'
-            // ],
+            [
+                name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins/', resourceLimitMemory: '512Mi'
+            ],
             [
                 name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
                 envVars: [
                     envVar(key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn')
                 ],
-                resourceLimitMemory: '1024Mi'
+                resourceLimitMemory: '1024Mi',
+                alwaysPullImage: true,
             ]
         ],
         volumes: [
