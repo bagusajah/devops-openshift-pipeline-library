6d8ab04c0c09b8bedece129edfdabfdeac4f7395
Mongkol Thongkraikaew <mongkol.tho@ascendcorp.com>
2018-03-19 15:57:06 +0700

    add module syncConfigurationServer

---
+++ vars/acnSyncConfigurationServer.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/groovy
+import com.ascendmoney.vulcan.Utils
+import java.io.File
+
+def call(body){
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def GLOBAL_VARS = config.global_vars
+  def LIST_ENV = config.list_env
+  def APP_VERSION = config.version
+  def S3_CONFIG_URL = "https://s3-ap-southeast-1.amazonaws.com/acm-aws-openshift-configuration-repo"
+  def GIT_HASH_FABRIC8_CONFIGURATION = ""
+
+  dir("/home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config") {
+    def env_list = ""
+    for(n = 0; n < LIST_ENV.size(); n++){
+      env_list = LIST_ENV[n]
+      sh "mkdir -p /home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION} && cd /home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}"
+      sh "curl -Ok ${S3_CONFIG_URL}/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip"
+      sh "mkdir -p /app-config/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}"
+      sh "cp -rf /home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.zip  /app-config/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/"
+       
+    } 
+  } 
+
+  return "Sync Configure Server Complete!"
+
+} 
