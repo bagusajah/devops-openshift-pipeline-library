e5467630f1c4c33a2c72f3c59b845464c235b5e9
Mongkol Thongkraikaew <mongkol.tho@ascendcorp.com>
2018-03-20 10:02:41 +0700

    add variable path config , directory_workspace

--- vars/acnSyncConfigurationServer.groovy
+++ vars/acnSyncConfigurationServer.groovy
@@ -12,17 +12,20 @@ def call(body){
   def LIST_ENV = config.list_env
   def APP_VERSION = config.version
   def COUNTRY_CODE =  config.country_code
+  def DIRECTORY_WORKSPACE = config.directory
+  def CONFIG_PATH = config.config_path
   def S3_CONFIG_URL = "https://s3-ap-southeast-1.amazonaws.com/acm-aws-openshift-configuration-repo"
   def GIT_HASH_FABRIC8_CONFIGURATION = ""
+
 
   
     def env_list = ""
     for(n = 0; n < LIST_ENV.size(); n++){
       env_list = LIST_ENV[n]
-      dir("/home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}") {
+      dir("${DIRECTORY_WORKSPACE}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}") {
       sh "curl -Ok ${S3_CONFIG_URL}/${COUNTRY_CODE}/${env_list}/${APP_NAME}/${APP_NAME}-${APP_VERSION}.zip"
-      sh "mkdir -p /app-configs/${COUNTRY_CODE}/${env_list}/${APP_NAME}"
-      sh "cp -rf /home/jenkins/workspace/${env.JOB_NAME}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}/${APP_NAME}-${APP_VERSION}.zip  /app-configs/${COUNTRY_CODE}/${env_list}/${APP_NAME}/"
+      sh "mkdir -p /${CONFIG_PATH}/${COUNTRY_CODE}/${env_list}/${APP_NAME}"
+      sh "cp -rf ${DIRECTORY_WORKSPACE}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}/${APP_NAME}-${APP_VERSION}.zip  /${CONFIG_PATH}/${COUNTRY_CODE}/${env_list}/${APP_NAME}/"
     } 
   } 
 
