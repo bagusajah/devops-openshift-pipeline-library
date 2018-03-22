649e4ef609a0024b441a506ca795f666ce5afe85
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-22 19:31:49 +0700

    init acnVerifyVersion

---
+++ vars/acnVerifyVersion.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/groovy
+import com.ascendmoney.cicd.Utils
+
+def call(body){
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def APP_URL_OPENSHIFT_FORMAT = config.app_url_openshift_format
+  def APP_VERSION = config.version
+  def GLOBAL_VARS = config.global_vars
+  def envList = config.envList
+
+  // def responseVersion = ""
+  def rs = ""
+
+  try {
+    timeout(time: 10, unit: 'MINUTES'){
+      waitUntil {
+        rs = restGetURL{
+          authString = ""
+          url = APP_URL_OPENSHIFT_FORMAT
+        }
+        sh "echo application version : ${rs.build.version}"
+        if (rs.build.version == APP_VERSION){
+          return true
+        }else {
+          return false
+        }
+      } // End waitUntil
+    }
+  }
+  catch(e) {
+    slackSend (channel: "${GLOBAL_VARS['CHANNEL_SLACK_NOTIFICATION']}", color: '#FF9900', message: "${env.JOB_NAME} build number ${env.BUILD_NUMBER} FAIL step \"Verify version application has changed\" on ${envList} environment. ${env.BUILD_URL}")
+    error "Pipeline aborted due to ${env.JOB_NAME} can not deploy version ${env.BUILD_NUMBER}"
+  }
+
+} // End Verify Version
