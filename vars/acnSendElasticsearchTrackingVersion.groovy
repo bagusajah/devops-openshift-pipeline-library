f6e3e3943e803ab82ecd274e06082663b8b788aa
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-03-23 10:18:46 +0700

    update

--- vars/acnSendElasticsearchTrackingVersion.groovy
+++ vars/acnSendElasticsearchTrackingVersion.groovy
@@ -40,7 +40,7 @@ def call(body){
     // sh "echo application version : ${rs.build.version}"
     // appVersion = rs.build.version
     echo "======= mock version ======="
-    version_mock = "1.0.1-84"
+    version_mock = "1.0.1-85"
     app_version = version_mock + "-retest"
   }
 
