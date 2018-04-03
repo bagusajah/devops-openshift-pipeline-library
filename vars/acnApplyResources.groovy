0e0358e603acd822677b880fd186eb17ad819fde
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-03 08:25:12 +0700

    revise

--- vars/acnApplyResources.groovy
+++ vars/acnApplyResources.groovy
@@ -2,21 +2,19 @@
 import com.ascendmoney.cicd.Utils
 import java.io.File
 
-
-
 def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
 
-    def utils = new Utils()
+  def utils = new Utils()
 
-   def artifact_data = config.artifact_data
-   def namespace =  config.namespace
-   def applicationType =  config.applicationType
+  def artifact_data = config.artifact_data
+  def namespace =  config.namespace
+  def applicationType =  config.applicationType
 
-   writeFile encoding: 'UTF-8', file: 'tmp_artifact_data_'+applicationType+'.yaml', text: artifact_data
-   sh "ls -lt"
-   sh "oc apply -f tmp_artifact_data_${applicationType}.yaml -n ${namespace}"
+  writeFile encoding: 'UTF-8', file: 'tmp_artifact_data_'+applicationType+'.yaml', text: artifact_data
+  sh "ls -lt"
+  sh "oc apply -f tmp_artifact_data_${applicationType}.yaml -n ${namespace}"
 }
