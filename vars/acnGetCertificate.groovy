866220a0d933be5e9f6010d2495ba97711c994c1
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-10 10:55:20 +0700

    change file cert

--- vars/acnGetCertificate.groovy
+++ vars/acnGetCertificate.groovy
@@ -12,12 +12,10 @@ def call(body) {
     def pathFile = config.pathFile
 
     def pathMounteCert = "/certs"
-    
-    sh "export LC_CTYPE=en_US.UTF-8"
-    sh "export LC_ALL=en_US.UTF-8"
-    sh "export CLIENT_KEY=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-client-key.key) ; perl -p -i -e \'s/#CLIENT_KEY#/\$ENV{CLIENT_KEY}/g\' ${pathFile}"
-    sh "export CLIENT_CERT=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-client-cert.crt) ; perl -p -i -e \'s/#CLIENT_CERT#/\$ENV{CLIENT_CERT}/g\' ${pathFile}"
-    sh "export CA_CERT=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-client-cert.crt) ; perl -p -i -e \'s/#CA_CERT#/\$ENV{CA_CERT}/g\' ${pathFile}"
+
+    sh "export LC_CTYPE=en_US.UTF-8 ; export LC_ALL=en_US.UTF-8 ; export CLIENT_KEY=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-client-key.key) ; perl -p -i -e \'s/#CLIENT_KEY#/\$ENV{CLIENT_KEY}/g\' ${pathFile}"
+    sh "export LC_CTYPE=en_US.UTF-8 ; export LC_ALL=en_US.UTF-8 ; export CLIENT_CERT=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-client-cert.crt) ; perl -p -i -e \'s/#CLIENT_CERT#/\$ENV{CLIENT_CERT}/g\' ${pathFile}"
+    sh "export LC_CTYPE=en_US.UTF-8 ; export LC_ALL=en_US.UTF-8 ; export CA_CERT=\$(sed -E ':a;N;\$!ba;s/\\r{0,1}\\n/\\\\r\\\\n/g' ${pathMounteCert}/${appScope}-ca-cert.crt) ; perl -p -i -e \'s/#CA_CERT#/\$ENV{CA_CERT}/g\' ${pathFile}"
     sh "cat ${pathFile}"
 
 } // End Function
