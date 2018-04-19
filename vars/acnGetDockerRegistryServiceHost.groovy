f6803a51cd8e5796526a861a702f9add78106114
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-19 17:33:06 +0700

    change command get ip

--- vars/acnGetDockerRegistryServiceHost.groovy
+++ vars/acnGetDockerRegistryServiceHost.groovy
@@ -3,8 +3,7 @@ import com.ascendmoney.cicd.Utils
 
 def call(body) {
 
-    def dockerRegistryHost = sh script: "getent hosts docker-registry.default.svc", returnStdout: true
-  	dockerRegistryHost = dockerRegistryHost.split("      ")[0]
+    def dockerRegistryHost = sh script: "getent hosts docker-registry.default.svc | awk '{print \$1}'", returnStdout: true
   	return dockerRegistryHost
 
 } // End Function
