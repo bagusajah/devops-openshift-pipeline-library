9403c12078954b60bda56ef725bafec27be308ec
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-20 18:51:18 +0700

    revise

--- vars/acnSyncConfigurationServer.groovy
+++ vars/acnSyncConfigurationServer.groovy
@@ -14,13 +14,14 @@ def call(body){
   def COUNTRY_CODE =  config.country_code
   def DIRECTORY_WORKSPACE = config.directory
   def CONFIG_PATH = config.config_path
-  def S3_CONFIG_URL = "https://s3-ap-southeast-1.amazonaws.com/acm-aws-openshift-configuration-repo"
   
   def env_list = ""
   for(n = 0; n < LIST_ENV.size(); n++){
     env_list = LIST_ENV[n]
     dir("${DIRECTORY_WORKSPACE}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}") {
-      sh "curl -Ok ${S3_CONFIG_URL}/${COUNTRY_CODE}/${env_list}/${APP_NAME}/${APP_NAME}-${APP_VERSION}.tar.gz"
+      withAWS(credentials:'openshift-profile-s3') {
+        s3Download bucket: 'acm-aws-openshift-configuration-repo', file: "${APP_NAME}-${APP_VERSION}.tar.gz", path: "${COUNTRY_CODE}/${env_list}/${APP_NAME}/${APP_NAME}-${APP_VERSION}.tar.gz"
+      }
       sh "mkdir -p ${CONFIG_PATH}/${COUNTRY_CODE}/${env_list}/${APP_NAME}"
       sh "cp -rf ${DIRECTORY_WORKSPACE}/s3-pull-config/tmp/${env_list}/${APP_NAME}-${APP_VERSION}/${APP_NAME}-${APP_VERSION}.tar.gz  ${CONFIG_PATH}/${COUNTRY_CODE}/${env_list}/${APP_NAME}/"
     } 
