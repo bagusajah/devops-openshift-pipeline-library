924187bdc17a84bcd3d8ca4d6a423b7bc8451d4d
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-26 17:33:33 +0700

    init send alert to webhook

---
+++ vars/acnSendAlertToWebhook.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/groovy
+import com.ascendmoney.cicd.Utils
+
+def call(body) {
+
+	def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def urlWebhook = config.urlWebhook
+    def envName = config.envName
+    def stageCurrent = config.stageCurrent
+    def appName = config.appName
+
+    String jsonAppnameTopic = "\"title\": \"Application Name\""
+    String jsonAppnameValue = "\"subtitle\": \"${appName}\""
+    String jsonImageJenkinsLogo = "\"imageUrl\": \"https://s3-ap-southeast-1.amazonaws.com/openshift-distributed-artifacts/scripts/jenkins.png\""
+    String jsonJobnameTopic = "\"topLabel\": \"Job Name\""
+    String jsonJobnameValue = "\"content\": \"${env.JOB_NAME}\""
+    String jsonBuildNumberTopic = "\"topLabel\": \"Build Number\""
+    String jsonBuildNumberValue = "\"content\": \"${env.BUILD_NUMBER}\""
+    String jsonPipelineStageTopic = "\"topLabel\": \"Pipeline Stage\""
+    String jsonPipelineStageValue = "\"content\": \"${stageCurrent}\""
+    String jsonJenkinsJobUrlTopic = "\"text\": \"Click For More Information\""
+    String jsonJenkinsJobUrlValue = "\"url\": \"${env.BUILD_URL}\""
+
+    String jsonStr = "{ \"cards\": [ { \"header\": { ${jsonAppnameTopic}, ${jsonAppnameValue}, ${jsonImageJenkinsLogo} }, \"sections\": [ { \"widgets\": [ { \"keyValue\": { ${jsonJobnameTopic}, ${jsonJobnameValue} }}, { \"keyValue\": { ${jsonBuildNumberTopic}, ${jsonBuildNumberValue} }}, { \"keyValue\": { ${jsonPipelineStageTopic}, ${jsonPipelineStageValue} }} ] }, { \"widgets\": [{ \"buttons\": [{ \"textButton\": { ${jsonJenkinsJobUrlTopic}, \"onClick\": { \"openLink\": { ${jsonJenkinsJobUrlValue} } } } }] }] } ] } ] }"
+    def object = new JsonBuilder(new JsonSlurper().parseText(jsonStr)).toPrettyString()
+
+    sh "curl -kX POST \"${urlWebhook}\" -H \"content-type: application/json\" -d '${object}'"
+
+} // End Function
