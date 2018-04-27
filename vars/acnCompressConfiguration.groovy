0363c07f2e27f9fe0141c45a7bc2c1667d9c08e3
kwanchanok.sku <kwanchanok.sku@ascendcorp.com>
2018-04-27 09:59:41 +0700

    revise acnCompressConfiguration

--- vars/acnCompressConfiguration.groovy
+++ vars/acnCompressConfiguration.groovy
@@ -8,29 +8,28 @@ def call(body){
   body.delegate = config
   body()
 
-  def GLOBAL_VARS = config.global_vars
+  def git_openshift_configuration = config.git_openshift_configuration
+  def app_name = config.app_name
+  def country_code = config.country_code
   def LIST_ENV = ["dev", "qa", "performance", "staging", "production"]
   def APP_VERSION = config.version
   def directory = config.directory
 
   def GIT_HASH_OPENSHIFT_CONFIGURATION = ""
 
-  def test_pwd = pwd()
-  echo "TEST_PWD ${test_pwd}"
-
-  git credentialsId: 'bitbucket-credential', url: GLOBAL_VARS['GIT_OPENSHIFT_CONFIGURATION']  
+  git credentialsId: 'bitbucket-credential', url: git_openshift_configuration  
   GIT_HASH_OPENSHIFT_CONFIGURATION = sh script: "cd ${directory}/update-config && git rev-parse --verify HEAD", returnStdout: true
   GIT_HASH_OPENSHIFT_CONFIGURATION = GIT_HASH_OPENSHIFT_CONFIGURATION.trim()
 
   def env_list = ""
   for(n = 0; n < LIST_ENV.size(); n++){
     env_list = LIST_ENV[n]
-    dir("${directory}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}"){
-      sh "cp -rf ${directory}/update-config/${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/* ${directory}/update-config/tmp/${env_list}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/"
-      sh "cd ${directory}/update-config/tmp/${env_list} && /bin/tar -zcvf \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.tar.gz\" \"${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}/\""
+    dir("${directory}/update-config/tmp/${env_list}/${app_name}-${APP_VERSION}"){
+      sh "cp -rf ${directory}/update-config/${country_code}/${env_list}/${app_name}/* ${directory}/update-config/tmp/${env_list}/${app_name}-${APP_VERSION}/"
+      sh "cd ${directory}/update-config/tmp/${env_list} && /bin/tar -zcvf \"${app_name}-${APP_VERSION}.tar.gz\" \"${app_name}-${APP_VERSION}/\""
       dir("${directory}/update-config/tmp/${env_list}"){
         withAWS(credentials:'openshift-s3-credential') {
-          s3Upload bucket: 'acm-aws-openshift-configuration-repo', file: "${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.tar.gz", path: "${GLOBAL_VARS['COUNTRY_CODE']}/${env_list}/${GLOBAL_VARS['APP_NAME']}/${GLOBAL_VARS['APP_NAME']}-${APP_VERSION}.tar.gz"
+          s3Upload bucket: 'acm-aws-openshift-configuration-repo', file: "${app_name}-${APP_VERSION}.tar.gz", path: "${country_code}/${env_list}/${app_name}/${app_name}-${APP_VERSION}.tar.gz"
         }
       } // End Upload zip file to s3
     } // End scope
