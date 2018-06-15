b49eee5c482480600544862045c55d56f792cbbb
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2018-06-15 16:09:54 +0700

    init override

---
+++ vars/acnOverrideLib.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/groovy
+import com.ascendmoney.cicd.Utils
+
+def call(body) {
+
+	def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def libName = config.libName
+    def libVersionExpect = config.libVersionExpect
+    def fileLibPath = config.fileLibPath
+
+    if ( libName == "mod-wsgi" ) {
+        result_no_specific_version = ""
+        result_specific_version = ""
+        try {
+            result_no_specific_version = sh script: "cat ${fileLibPath} |grep -e \"mod-wsgi\$\" ", returnStdout: true
+            result_no_specific_version = result_no_specific_version.length()
+        }
+        catch(Exception e) {
+            result_no_specific_version = 0
+        }
+        result_specific_version = sh script: "cat ${fileLibPath} |grep -e \"mod-wsgi[\$=]\"", returnStdout: true
+        result_specific_version = result_specific_version.length()
+        if ( result_no_specific_version.toInteger() > 0 ) {
+            line_full = sh script: "cat ${fileLibPath} |grep -n \"mod-wsgi\$\"", returnStdout: true
+            line = line_full.substring(0, line_full.lastIndexOf(":"))
+            sh "sed -i ''${line}'s/mod.*/${libVersionExpect}/g' ${fileLibPath}"
+        }
+        if ( result_specific_version.toInteger() > 0 ) {
+            line_full = sh script: "cat ${fileLibPath} |grep -n \"mod-wsgi[\$=]\"", returnStdout: true
+            line = line_full.substring(0, line_full.lastIndexOf(":"))
+            sh "sed -i ''${line}'s/mod.*/${libVersionExpect}/g' ${fileLibPath}"
+        }
+    }
+
+  	return domainName
+
+} // End Function
