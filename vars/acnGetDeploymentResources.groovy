971d0952368c33932936dd61724bb77b07f44711
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2018-06-19 10:25:30 +0700

    update

--- vars/acnGetDeploymentResources.groovy
+++ vars/acnGetDeploymentResources.groovy
@@ -55,8 +55,10 @@ def call(body) {
         }
         echo "responseGetPVCLength ${responseGetPVCLength}"
         if ( responseGetPVCLength.toInteger() == 0 ) {
+            sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
+            sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
             responseDeploy = applyResourceYaml {
-                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc.yaml"
+                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
                 namespaceEnv = namespace_env
                 kind = "pvc"
                 app_name = appName
@@ -75,9 +77,11 @@ def call(body) {
             }
             echo "responseGetNetworkPolicyLength ${responseGetNetworkPolicyLength}"
             if ( responseGetNetworkPolicyLength.toInteger() == 0 ) {
-                sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                 responseDeploy = applyResourceYaml {
-                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                     namespaceEnv = namespace_env
                     kind = "networkpolicy"
                     app_name = appName
@@ -110,8 +114,10 @@ def call(body) {
                 if ( routeTLSEnable == "true" ) {
                     routeType = "route-tls"
                 }
+                sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                 responseDeploy = applyResourceYaml {
-                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                     namespaceEnv = namespace_env
                     kind = "route"
                     app_name = appName
@@ -121,8 +127,10 @@ def call(body) {
     } else {
         // Deploy PVC
         if ( applicationType != 'mountebank' && forceDeployList[7] == "true" ) {
+            sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
+            sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
             responseDeploy = applyResourceYaml {
-                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc.yaml"
+                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/pvc-replace.yaml"
                 namespaceEnv = namespace_env
                 kind = "pvc"
                 app_name = appName
@@ -148,9 +156,11 @@ def call(body) {
             // Deploy NETWORK POLICY
             echo "start networkpolicy"
             if ( applicationType != 'mountebank' && forceDeployList[6] == "true" ) {
-                sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                 responseDeploy = applyResourceYaml {
-                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                     namespaceEnv = namespace_env
                     kind = "networkpolicy"
                     app_name = appName
@@ -164,9 +174,11 @@ def call(body) {
                 }
                 echo "responseGetNetworkPolicyLength ${responseGetNetworkPolicyLength}"
                 if ( responseGetNetworkPolicyLength.toInteger() == 0 ) {
-                    sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                    sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                    sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
+                    sh "sed -i \"s~'#ENV_NAME#'~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                     responseDeploy = applyResourceYaml {
-                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy.yaml"
+                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/networkpolicy-replace.yaml"
                         namespaceEnv = namespace_env
                         kind = "networkpolicy"
                         app_name = appName
@@ -185,11 +197,13 @@ def call(body) {
                     appScope = app_scope
                     pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
                 }
-                sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
-                sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
-                sh "sed -i \"s~'#ROUTE_HOSTNAME#'~${domainName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                sh "sed -i \"s~'#ROUTE_HOSTNAME#'~${domainName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                 responseDeploy = applyResourceYaml {
-                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                    pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                     namespaceEnv = namespace_env
                     kind = "route"
                     app_name = appName
@@ -206,15 +220,17 @@ def call(body) {
                     if ( routeTLSEnable == "true" ) {
                         routeType = "route-tls"
                     }
+                    sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                    sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                     certList = acnGetCertificate{
                         appScope = app_scope
-                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                     }
-                    sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
-                    sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
-                    sh "sed -i \"s~'#ROUTE_HOSTNAME#'~${domainName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                    sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                    sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
+                    sh "sed -i \"s~'#ROUTE_HOSTNAME#'~${domainName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                     responseDeploy = applyResourceYaml {
-                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}.yaml"
+                        pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/application/${routeType}-replace.yaml"
                         namespaceEnv = namespace_env
                         kind = "route"
                         app_name = appName
@@ -222,9 +238,11 @@ def call(body) {
                 }
             } else if ( applicationType == 'mountebank' ) {
                 if ( namespace_env.contains("dev") ) {
-                    sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route.yaml"
-                    sh "sed -i \"s~'#MB_ROUTE_HOSTNAME#'~${domainName}~g\" pipeline/${platformType}/${versionOpenshift}/mountebank/route.yaml"
-                    sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route.yaml"
+                    sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
+                    sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
+                    sh "sed -i \"s~#ENV_NAME#~${envName}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
+                    sh "sed -i \"s~'#MB_ROUTE_HOSTNAME#'~${domainName}~g\" pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
+                    sh "sed -i \"s~'#COUNTRY_CODE#'~${countryCode}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
                     container(name: 'jnlp'){
                         responseGetRoute = sh script: "oc get route -l appName=${appName} -n ${namespace_env} |grep ${appName} |awk \'{print \$1}\'", returnStdout: true
                         responseGetRouteLength = responseGetRoute.length()
@@ -232,7 +250,7 @@ def call(body) {
                         if ( responseGetRouteLength.toInteger() == 0 ) {
                             echo "Route ${appName} is not existing"
                             responseDeploy = applyResourceYaml {
-                                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route.yaml"
+                                pathFile = "${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/route-replace.yaml"
                                 namespaceEnv = namespace_env
                                 kind = "route"
                                 app_name = appName
@@ -253,14 +271,21 @@ def call(body) {
     }
     
     if ( applicationType != 'mountebank') {
-        sh "sed -i \"s~'#NUM_OF_REPLICA#'~${replicaNum}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig.yaml"
-        sh "sed -i \"s~'#ROLLING_UPDATE_SURGE#'~${rollingUpdateSurge}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig.yaml"
-        sh "sed -i \"s~'#ROLLING_UPDATE_UNAVAILABLE#'~${rollingUpdateUnavailable}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig.yaml"
+        sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig-replace.yaml"
+        sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#NUM_OF_REPLICA#'~${replicaNum}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#ROLLING_UPDATE_SURGE#'~${rollingUpdateSurge}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#ROLLING_UPDATE_UNAVAILABLE#'~${rollingUpdateUnavailable}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/application/deploymentconfig-replace.yaml"
     } else {
-        sh "sed -i \"s~'#DEFAULT_NUM_REPLICA_MB#'~${replicaNum}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig.yaml"
-        sh "sed -i \"s~'#MOUNTEBANK_SURGE#'~${rollingUpdateSurge}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig.yaml"
-        sh "sed -i \"s~'#MOUNTEBANK_UNAVAILABLE#'~${rollingUpdateUnavailable}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig.yaml"
+        sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig-replace.yaml"
+        sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#DEFAULT_NUM_REPLICA_MB#'~${replicaNum}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#MOUNTEBANK_SURGE#'~${rollingUpdateSurge}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig-replace.yaml"
+        sh "sed -i \"s~'#MOUNTEBANK_UNAVAILABLE#'~${rollingUpdateUnavailable}~g\" ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/deploymentconfig-replace.yaml"
     }
+
+    sh "rm -rf ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/service-replace.yaml"
+    sh "cp ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/service.yaml ${directory}/pipeline/${platformType}/${versionOpenshift}/mountebank/service-replace.yaml"
 
     def list = """
 ---
@@ -268,7 +293,7 @@ apiVersion: v1
 kind: List
 items:
 """
-    def deploymentYaml = readFile encoding: 'UTF-8', file: directory + "/pipeline/" + platformType + "/" + versionOpenshift + "/" + applicationType + "/" + "deploymentconfig.yaml"
+    def deploymentYaml = readFile encoding: 'UTF-8', file: directory + "/pipeline/" + platformType + "/" + versionOpenshift + "/" + applicationType + "/" + "deploymentconfig-replace.yaml"
     deploymentYaml = deploymentYaml.replaceAll(/'#ENV_NAME#'/, envName)
     deploymentYaml = deploymentYaml.replaceAll(/'#APP_VERSION#'/, appVersion)
     deploymentYaml = deploymentYaml.replaceAll(/'#IMAGE_URL#'/, imageName)
@@ -278,7 +303,7 @@ items:
     deploymentYaml = deploymentYaml.replaceAll(/'#RUNWAY_NAME#'/, runwayName) + """
 
 """
-    def serviceYaml = readFile encoding: 'UTF-8', file: directory + "/pipeline/" + platformType + "/"  + versionOpenshift + '/' + applicationType + '/service.yaml'
+    def serviceYaml = readFile encoding: 'UTF-8', file: directory + "/pipeline/" + platformType + "/"  + versionOpenshift + '/' + applicationType + '/service-replace.yaml'
     serviceYaml = serviceYaml.replaceAll(/'#COUNTRY_CODE#'/, countryCode)
     serviceYaml = serviceYaml.replaceAll(/'#ENV_NAME#'/, envName) + """
 
