b68f299f46a4904c43fd0821d0a5ba311c1d01e4
Kwanchanok Skuljarernpon <kwanchanok.sku@ascendcorp.com>
2018-06-29 15:14:39 +0700

    update

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -7,17 +7,34 @@ def call(body) {
     body.delegate = config
     body()
 
+    def actor = ""
+    def cause = ""
+    def action = ""
+
     def timeoutTime= config.timeoutTime ?: 30
     def proceedMessage = """Would you like to promote version ${config.version} to the next environment?
 """
-    try {
-        timeout(time:timeoutTime, unit:'MINUTES') {
-            input id: 'Proceed', message: "\n${proceedMessage}"
+    timeout(time:timeoutTime, unit:'MINUTES'){
+        try {
+            input message: proceedMessage, ok: 'Approve'
+            action = "approve"
         }
-        echo "approve"
-    } catch(Exception e) {
-        return true
+        catch(org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+            action = "abort_or_nothing"
+            cause = e.causes.get(0)
+            actor = cause.getUser().toString()
+        }
+        finally {
+            // if approve --> go on
+            // if abort --> stop
+            // if no action --> go on
+            if ( actor == "SYSTEM" || action == "approve" ) {
+                return true
+            } else if ( actor != "SYSTEM" && actor = "abort_or_nothing" ) {
+                return false
+            }
+        }
+        
     }
-    return true
 
 }
